4a0ef58a01252218fe3fbe359cd08cfa0406ff56
Oleksandr Kryvonos <geraclion@gmail.com>
2023-07-09 10:19:30 +0200

    some changes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿// start 2023-06-10 17:02 CET, Munich
+// new version 2023-07-08 Munich
 using System.Reflection;
 using System.Text.Json;
 
 
-class TestClass
+class Lisperanto
 {
     static Dictionary<string, List<string>> arguments_needed_to_be_evaluated_for_function = new Dictionary<string, List<string>>();
 
@@ -120,10 +121,7 @@ static Dictionary<string, object> evaluate_element(EvaluationState state)
         }
     }
     return new Dictionary<string, object>();
-
-
 }
-
 
     static async Task<JsonElement> evaluate_file(string path)
     {
@@ -141,14 +139,6 @@ static Dictionary<string, object> evaluate_element(EvaluationState state)
     }
 }
 
-
-
-
-
-
-
-
-
 class EvaluationState 
 {
     public JsonElement starting_element;
@@ -156,105 +146,3 @@ class EvaluationState
     public List<EvaluationState> dependencies = new List<EvaluationState>();
     
 }
-
-
-
-// List<string> prepare_transpiled(JsonElement key, bool is_inside_block)
-// {
-//     List<string> result = new List<string>();
-//     if (key.ValueKind == JsonValueKind.Object)
-//     {
-//         var type = key.GetProperty("type").GetString();
-//         if (type == "function-call")
-//         {
-//             result.AddRange(transpile_function_call(key, is_inside_block));
-//             return result;
-//         }
-        
-//         if (type == "code-block")
-//         {
-//             result.Add("{");
-//             foreach(var inner_key in key.GetProperty("statements").EnumerateArray())
-//             {
-//                 result.AddRange(prepare_transpiled(inner_key, true).Select(q => "    " + q));
-
-//             }
-//             result.Add("}");
-//             return result;
-//         }
-//         if (type == "function-definition")
-//         {
-//             result.AddRange(transpile_function_definition(key));
-//             return result;
-//         }
-//         if (type == "return-statement")
-//         {
-//             result.Add("return");
-//             result.AddRange(prepare_transpiled(key.GetProperty("return-expression"), false).Select(q => "    " + q));
-//             result.Add(";");
-//             return result;
-//         }
-//         if (type == "variable-declaration")
-//         {
-//             result.Add($"{key.GetProperty("variable-type").GetString()} {key.GetProperty("variable-name").GetString()} =");
-//             result.AddRange(prepare_transpiled(key.GetProperty("value-expression"), false).Select(q => "    " + q));
-//             result.Add(";");
-//             return result;
-//         }
-//         if (type == "path-reference")
-//         {
-//             var path = key.GetProperty("path").EnumerateArray().ToList();
-//             var transformed_path = path.Select( (q, i) => i == (path.Count - 1) ? q.GetString() : $"{q.GetString()}.");
-//             result.AddRange(transformed_path);
-//             return result;
-//         }
-//     }
-//     result.Add(key.GetRawText());
-//     return result;
-// }
-
-// List<string> transpile_function_definition(JsonElement key)
-// {
-//     List<string> result = new List<string>();
-//     result.Add($"{key.GetProperty("return-type")} {key.GetProperty("name").GetString()}(");
-//     var parameters = key.GetProperty("parameters").EnumerateArray().ToArray();
-//     for (var index = 0; index < parameters.Length; index++)
-//     {
-//         var inner_key = parameters[index];
-//         if (inner_key.ValueKind == JsonValueKind.Object
-//             && inner_key.GetProperty("type").GetString() == "parameter-declaration"
-//             )
-//         {
-//             var comma_possibly = index == parameters.Length - 1 ? "" : ",";
-//             var parameter_type = inner_key.GetProperty("parameter-type").GetString();
-//             var parameter_name = inner_key.GetProperty("parameter-name").GetString();
-//             result.Add($"    {parameter_type} {parameter_name}{comma_possibly}");
-
-//         }
-//     }
-//     result.Add(")");
-//     result.AddRange(prepare_transpiled(key.GetProperty("body"), false));
-//     return result;
-// }
-
-// List<string> transpile_function_call(JsonElement key, bool is_inside_block)
-// {
-//     var result = new List<string>();
-//     result.AddRange(prepare_transpiled(key.GetProperty("function-object"), false));
-//     // 2023-06-10 ~18:00 I can do evaluation as well as code generation
-//     // 2023-06-10 18:34 but lets finish with generation first
-//     var transformed_parameters = key.GetProperty("arguments").EnumerateArray().Select(q => prepare_transpiled(q, false)).ToArray();
-//     var semicolon_possibly = is_inside_block? ";" : "";
-//     result.Add($"(");
-//     for(var i = 0; i < transformed_parameters.Length; i++)
-//     {
-//         var inner = transformed_parameters[i];
-//         result.AddRange(inner.Select(q => "    " + q));
-//         if ( i != transformed_parameters.Length - 1)
-//         {
-//             result[result.Count - 1] = result[result.Count - 1] + ",";
-//         }
-//     }
-//     result.Add($"){semicolon_possibly}");
-//     return result;
-// }
