dd452cc782477c3038a6dc29c6ed8d460fa67d14
Oleksandr Kryvonos <geraclion@gmail.com>
2023-11-17 21:39:43 +0100

    remove the code I found on Internet

--- Program.cs
+++ Program.cs
@@ -11,6 +11,28 @@ using System.Net;
 
 class Lisperanto
 {
+    private static void streamReadExample()
+    {
+        // if (request.HasEntityBody)
+        //     {
+        //         var body = request.InputStream;
+        //         var encoding = request.ContentEncoding;
+        //         var reader = new StreamReader(body, encoding);
+        //         if (request.ContentType != null)
+        //         {
+        //             Console.WriteLine("Client data content type {0}", request.ContentType);
+        //         }
+        //         Console.WriteLine("Client data content length {0}", request.ContentLength64);
+
+        //         Console.WriteLine("Start of data:");
+        //         string s = reader.ReadToEnd();
+        //         Console.WriteLine(s);
+        //         Console.WriteLine("End of data:");
+        //         reader.Close();
+        //         body.Close();
+                
+        //     }
+    }
     static async Task Main(string[] args)
     {
         var _listener = new HttpListener();
@@ -30,25 +52,48 @@ class Lisperanto
             Console.WriteLine($"{request.Url}");
             Console.WriteLine(request.Url.AbsolutePath);
             var requested_path = Path.Combine(root_path, "www-root", request.Url.AbsolutePath.Substring(1));
-            var file_content = await File.ReadAllBytesAsync(requested_path);
-            context.Response.ContentType = "text/html";
-            context.Response.OutputStream.Write(file_content, 0, file_content.Length);
+            try
+            {
+                context.Response.AddHeader("Server", "web-server-created-by-Oleksandr-Kryvonos/v-2023-11-17");
+                context.Response.AddHeader("Date", DateTime.Now.ToString("yyyy-MM-dd--HH:mm:ss"));
+                if (File.Exists(requested_path) == false)
+                {
+                    context.Response.StatusCode = (int) HttpStatusCode.NotFound;
+                    context.Response.Close();
+                    continue;
+                }
+                var file_content = await File.ReadAllBytesAsync(requested_path);
+                var extension = Path.GetExtension(request.Url.AbsolutePath);
+                Dictionary<string, string> lookup_for_extension = new Dictionary<string, string>();
+                lookup_for_extension.Add(".html", "text/html");
+                lookup_for_extension.Add(".js", "application/javascript");
+                lookup_for_extension.Add(".txt", "text/plain");
+                if (lookup_for_extension.ContainsKey(extension))
+                {
+                    context.Response.ContentType = lookup_for_extension[extension];
+                }
+                else
+                {
+                    context.Response.ContentType = "text/plain";
+                }
+                
+                context.Response.OutputStream.Write(file_content, 0, file_content.Length);
+                
+                context.Response.StatusCode = (int) HttpStatusCode.OK;
+                context.Response.Close();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                context.Response.Close();
+                continue;
+            }
             
-            context.Response.StatusCode = (int) HttpStatusCode.OK;
-            context.Response.Close();
         
 
         }
-        
-        // WebHost.CreateDefaultBuilder(args)
-        //     .Configure(config => 
-        //     {
-        //         config.UseStaticFiles();
-        //     }
-                
-        //         )
-        //     .UseWebRoot("www-root")
-        //     .Build().Run();
 
     }
 }
